18cd34ff0f894e018fd85af35e6ac91339046ee9
ehoang0106 <khoahoang1698@gmail.com>
2023-10-30 21:17:09 -0700

    update AbstractQueue.py nothing was changed just add comment to some lines of code

--- Homework/Homework5/AbstractQueue.py
+++ Homework/Homework5/AbstractQueue.py
@@ -20,12 +20,13 @@ class ArrayQueue(AbstractCollection):
 
     def ensure_capacity(self):
         if self._size == len(self._items):
-            temp_arr = Array(len(self._items)*10)
+            temp_arr = Array(len(self._items)*10) #increase temporay array size x10 original array
             f = self._front
 
             for i in range(self._size):
                 temp_arr[i] = self._items[f]
-                f =(f+1) % len(self._items)
+
+                f =(f+1) % len(self._items) #f+1 is increment the index "front" then % len(self._items) to make sure if we reached the last element which is index 9 (size array is 10), the next logical positen after 9 would be index 0 instead of 10.
 
             self._front = 0
             self._back = self._size
@@ -67,8 +68,11 @@ class ArrayQueue(AbstractCollection):
 
     def print_queue(self):
         f = self._front
+        if self.is_empty():
+            print("Queue is empty!")
+        else:
 
-        for i in range(self._size):
-            print(self._items[f])
-            f = (f+1) % len(self._items)
+            for i in range(self._size):
+                print(self._items[f])
+                f = (f+1) % len(self._items)
 
